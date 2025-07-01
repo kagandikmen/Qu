e43540251fbf3b0fa5d004c8ae8c85b98bd4abe7
Kagan Dikmen <kagandikmen@outlook.com>
2025-07-01 07:42:23 +0200

    Reimplement `qu_core` as topmost module
    This commit reimplements the `qu_core` module to function as the topmost
    module describing the interactions between front-end, back-end, register
    file and reservation station. The back-end is yet to be implemented.

--- hdl/rtl/qu_core.sv
+++ hdl/rtl/qu_core.sv
@@ -1,6 +1,6 @@
 // The Qu Processor CPU core module
 // Created:     2025-06-27
-// Modified:    2025-06-28
+// Modified:    2025-07-01
 
 // Copyright (c) 2025 Kagan Dikmen
 // SPDX-License-Identifier: MIT
@@ -8,6 +8,9 @@
 
 `ifndef QU_CORE
 `define QU_CORE
+
+`include "../lib/qu_common.svh"
+`include "../lib/qu_uop.svh"
 
 import qu_common::*;
 import qu_uop::*;
@@ -18,109 +21,110 @@ module qu_core
         parameter INSTR_WIDTH = QU_INSTR_WIDTH,
         parameter PC_WIDTH = QU_PC_WIDTH
     )(
-        input logic clk,
-        input logic rst,
+        input   logic clk,
+        input   logic rst,
         
-        input logic branch,
-        input logic jump,
-        input logic exception,
-        input logic stall,
-        input logic [PC_WIDTH-1:0] pc_override,
+        input   logic branch,
+        input   logic jump,
+        input   logic exception,
+        input   logic stall,
+        input   logic [PC_WIDTH-1:0] pc_override,
 
-        input logic if_stall,
-        input logic id_stall,
+        input   logic if_stall,
+        input   logic id_stall,
+        input   logic mp_stall,
 
-        output logic nop,
-        output logic invalid,
-        output uop_t uop_out
+        input   logic rf_wr_en,
+        input   logic [PHY_RF_ADDR_WIDTH-1:0] rf_rd_addr,
+        input   logic [31:0] rf_data_in,
+
+        input   res_st_addr_t res_st_rd1_addr_in,
+        output  res_st_cell_t res_st_rd1_out,
+        input   res_st_addr_t res_st_rd2_addr_in,
+        output  res_st_cell_t res_st_rd2_out
     );
 
-    logic fetch_branch_in;
-    logic fetch_jump_in;
-    logic fetch_exception_in;
-    logic fetch_stall_in;
-    logic [PC_WIDTH-1:0] fetch_pc_override_in;
-    logic [INSTR_WIDTH-1:0] fetch_instr_out;
-    logic [PC_WIDTH-1:0] fetch_pc_out;
+    logic [PHY_RF_ADDR_WIDTH-1:0] front_end_rf_rs1_addr_out;
+    logic [PHY_RF_ADDR_WIDTH-1:0] front_end_rf_rs2_addr_out;
+    logic [31:0] front_end_rf_rs1_data_in;
+    logic [31:0] front_end_rf_rs2_data_in;
+    logic front_end_res_st_wr_en_out;
+    res_st_addr_t front_end_res_st_wr_addr_out;
+    res_st_cell_t front_end_res_st_data_out;
 
-    logic [INSTR_WIDTH-1:0] decode_instr_in;
-    logic decode_nop_out;
-    logic decode_invalid_out;
-    uop_t decode_uop_out;
+    logic res_st_wr_en_in;
+    res_st_addr_t res_st_wr_addr_in;
+    res_st_cell_t res_st_wr_in;
 
-    logic fifo_rd_en_in;
-    logic [INSTR_WIDTH-1:0] fifo_data_out;
-    logic fifo_wr_en_in;
-    logic [INSTR_WIDTH-1:0] fifo_data_in;
-    logic fifo_empty_out;
-    logic fifo_full_out;
+    logic [PHY_RF_ADDR_WIDTH-1:0] rf_rs1_addr_in;
+    logic [PHY_RF_ADDR_WIDTH-1:0] rf_rs2_addr_in;
+    logic [31:0] rf_rs1_data_out;
+    logic [31:0] rf_rs2_data_out;
 
-    logic startup_ctrl_if_en_out;
-    logic startup_ctrl_id_en_out;
+    assign front_end_rf_rs1_data_in = rf_rs1_data_out;
+    assign front_end_rf_rs2_data_in = rf_rs2_data_out;
 
-    fetch #(
-        .INSTR_WIDTH(INSTR_WIDTH),
+    assign res_st_wr_en_in = front_end_res_st_wr_en_out;
+    assign res_st_wr_addr_in = front_end_res_st_wr_addr_out;
+    assign res_st_wr_in = front_end_res_st_data_out;
+
+    assign rf_rs1_addr_in = front_end_rf_rs1_addr_out;
+    assign rf_rs2_addr_in = front_end_rf_rs2_addr_out;
+
+    front_end #(
         .PMEM_INIT_FILE(PMEM_INIT_FILE),
-        .PC_WIDTH(PC_WIDTH),
-        .PC_RESET_VAL(QU_PC_RESET_VAL)
-    ) qu_fetch (
+        .INSTR_WIDTH(INSTR_WIDTH),
+        .PC_WIDTH(PC_WIDTH)
+    ) qu_front_end (
         .clk(clk),
         .rst(rst),
-        .branch(fetch_branch_in),
-        .jump(fetch_jump_in),
-        .exception(fetch_exception_in),
-        .stall(fetch_stall_in),
-        .pc_override_in(fetch_pc_override_in),
-        .instr(fetch_instr_out),
-        .pc(fetch_pc_out)
+        .branch(branch),
+        .jump(jump),
+        .exception(exception),
+        .stall(stall),
+        .pc_override(pc_override),
+        .if_stall(if_stall),
+        .id_stall(id_stall),
+        .mp_stall(mp_stall),
+        .rf_rs1_addr(front_end_rf_rs1_addr_out),
+        .rf_rs2_addr(front_end_rf_rs2_addr_out),
+        .rf_rs1_data_in(front_end_rf_rs1_data_in),
+        .rf_rs2_data_in(front_end_rf_rs2_data_in),
+        .res_st_wr_en_out(front_end_res_st_wr_en_out),
+        .res_st_wr_addr_out(front_end_res_st_wr_addr_out),
+        .res_st_data_out(front_end_res_st_data_out)
     );
 
-    fifo #(
-        .FIFO_WIDTH(INSTR_WIDTH),
-        .FIFO_DEPTH(4)
-    ) qu_fifo_if_id (
+    res_st #(
+        .RES_ST_DEPTH(RES_ST_DEPTH)
+    ) qu_res_st (
         .clk(clk),
         .rst(rst),
-        .rd_en(fifo_rd_en_in),
-        .data_out(fifo_data_out),
-        .wr_en(fifo_wr_en_in),
-        .data_in(fifo_data_in),
-        .empty(fifo_empty_out),
-        .almost_empty(),
-        .full(fifo_full_out),
-        .almost_full()
+        .wr_en(res_st_wr_en_in),
+        .wr_addr(res_st_wr_addr_in),
+        .wr_in(res_st_wr_in),
+        .rd1_addr(res_st_rd1_addr_in),
+        .rd1_out(res_st_rd1_out),
+        .rd2_addr(res_st_rd2_addr_in),
+        .rd2_out(res_st_rd2_out)
     );
 
-    decode #(
-        .INSTR_WIDTH(INSTR_WIDTH)
-    ) qu_decode (
-        .instr_in(decode_instr_in),
-        .nop(decode_nop_out),
-        .invalid(decode_invalid_out),
-        .uop_out(decode_uop_out)
-    );
-
-    startup_ctrl qu_startup_ctrl (
+    rf #(
+        .RF_WIDTH(32),
+        .RF_DEPTH(PHY_RF_DEPTH)
+    ) qu_phy_rf (
         .clk(clk),
         .rst(rst),
-        .if_en(startup_ctrl_if_en_out),
-        .id_en(startup_ctrl_id_en_out)
+        .rs1_addr(rf_rs1_addr_in),
+        .rs2_addr(rf_rs2_addr_in),
+        .rs1_data_out(rf_rs1_data_out),
+        .rs2_data_out(rf_rs2_data_out),
+        .wr_en(rf_wr_en),
+        .rd_addr(rf_rd_addr),
+        .data_in(rf_data_in)
     );
-    
-    assign fetch_branch_in = branch;
-    assign fetch_jump_in = jump;
-    assign fetch_exception_in = exception;
-    assign fetch_stall_in = stall || if_stall || fifo_full_out;
-    assign fetch_pc_override_in = pc_override;
-    
-    assign fifo_rd_en_in = !stall && !id_stall && !fifo_empty_out && startup_ctrl_id_en_out;
-    assign fifo_wr_en_in = !fetch_stall_in && startup_ctrl_if_en_out;
-    assign fifo_data_in = fetch_instr_out;
-    
-    assign decode_instr_in = fifo_data_out;
-    assign nop = decode_nop_out;
-    assign invalid = decode_invalid_out;
-    assign uop_out = decode_uop_out;
+
+    // TODO: Add back-end here when it is ready
 
 endmodule
 
--- hdl/sim/tb_qu_core.sv
+++ hdl/sim/tb_qu_core.sv
@@ -1,12 +1,15 @@
 // The Qu Processor CPU core testbench
 // Created:     2025-06-27
-// Modified:    2025-06-28
+// Modified:    2025-07-01
 
 // Copyright (c) 2025 Kagan Dikmen
 // SPDX-License-Identifier: MIT
 // See LICENSE for details
 
 `timescale 1ns/1ps
+
+`include "../lib/qu_common.svh"
+`include "../lib/qu_uop.svh"
 
 import qu_common::*;
 import qu_uop::*;
@@ -30,10 +33,16 @@ module tb_qu_core
 
     logic if_stall;
     logic id_stall;
+    logic mp_stall;
 
-    logic nop;
-    logic invalid;
-    uop_t uop_out;
+    logic rf_wr_en;
+    logic [PHY_RF_ADDR_WIDTH-1:0] rf_rd_addr;
+    logic [31:0] rf_data_in;
+
+    res_st_addr_t res_st_rd1_addr_in;
+    res_st_cell_t res_st_rd1_out;
+    res_st_addr_t res_st_rd2_addr_in;
+    res_st_cell_t res_st_rd2_out;
 
     qu_core #(
         .PMEM_INIT_FILE(PMEM_INIT_FILE),
@@ -49,9 +58,14 @@ module tb_qu_core
         .pc_override(pc_override),
         .if_stall(if_stall),
         .id_stall(id_stall),
-        .nop(nop),
-        .invalid(invalid),
-        .uop_out(uop_out)
+        .mp_stall(mp_stall),
+        .rf_wr_en(rf_wr_en),
+        .rf_rd_addr(rf_rd_addr),
+        .rf_data_in(rf_data_in),
+        .res_st_rd1_addr_in(res_st_rd1_addr_in),
+        .res_st_rd1_out(res_st_rd1_out),
+        .res_st_rd2_addr_in(res_st_rd2_addr_in),
+        .res_st_rd2_out(res_st_rd2_out)
     );
 
     always #5   clk = ~clk;
@@ -67,32 +81,18 @@ module tb_qu_core
         pc_override <= 'd0;
         if_stall <= 1'b0;
         id_stall <= 1'b0;
+        mp_stall <= 1'b0;
+        rf_wr_en <= 1'b0;
+        rf_rd_addr <= 'd0;
+        rf_data_in <= 'd0;
+        res_st_rd1_addr_in <= 'd0;
+        res_st_rd2_addr_in <= 'd0;
 
         @(posedge clk);
         rst <= 1'b1;
 
         repeat(5) @(posedge clk);
         rst <= 1'b0;
-
-        repeat(10) @(posedge clk);
-        if_stall <= 1'b1;
-
-        repeat(20) @(posedge clk);
-        if_stall <= 1'b0;
-
-        repeat(3) @(posedge clk);
-        id_stall <= 1'b1;
-
-        repeat(20) @(posedge clk);
-        id_stall <= 1'b0;
-
-        repeat(3) @(posedge clk);
-        branch <= 1'b1;
-        pc_override <= 'd4;
-
-        @(posedge clk);
-        branch <= 1'b0;
-        pc_override <= 'd0;
 
         #200;
         $finish;
