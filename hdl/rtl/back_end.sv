d4d3a7338424380aec56a291bf53d96042bea54b
Kagan Dikmen <kagandikmen@outlook.com>
2025-07-06 20:56:53 +0200

    Adapt top back-end module for retire stage

--- hdl/rtl/back_end.sv
+++ hdl/rtl/back_end.sv
@@ -1,6 +1,6 @@
 // Top back-end module of The Qu Processor
 // Created:     2025-07-03
-// Modified:    2025-07-04
+// Modified:    2025-07-06
 
 // Copyright (c) 2025 Kagan Dikmen
 // SPDX-License-Identifier: MIT
@@ -21,6 +21,7 @@ module back_end
         input   logic rst,
         input   logic schedule_en,
 
+        // reservation station read interface
         output  res_st_addr_t res_st_rd1_addr,
         input   res_st_cell_t res_st_rd1_in,
         output  res_st_addr_t res_st_rd2_addr,
@@ -30,8 +31,25 @@ module back_end
         output  res_st_addr_t res_st_rd4_addr,
         input   res_st_cell_t res_st_rd4_in,
 
-        output  logic [31:0] value_out,
-        output  res_st_cell_t op_out
+        // reservation station retire interface
+        output  logic res_st_retire_en,
+        output  rob_addr_t res_st_retire_rob_addr,
+        output  phy_rf_data_t res_st_retire_value,
+
+        // register file writeback interface
+        output  logic phy_rf_wr_en,
+        output  phy_rf_addr_t phy_rf_wr_addr,
+        output  phy_rf_data_t phy_rf_wr_data,
+
+        // busy table interface
+        output  logic busy_table_wr_en,
+        output  phy_rf_addr_t busy_table_wr_addr,
+        output  logic busy_table_wr_data,
+
+        // rename stage interface
+        output  rob_addr_t rob_tail_ptr,
+        input   logic rob_incr_tail_ptr,
+        output  logic rob_full
     );
 
     res_st_addr_t schedule_res_st_rd1_addr_out;
@@ -56,6 +74,28 @@ module back_end
     logic [31:0] execute_value_out;
     res_st_cell_t execute_op_out;
 
+    logic fifo_ex_rt_rd_en;
+    logic [$bits(res_st_cell_t)+32-1:0] fifo_ex_rt_data_out;
+    logic fifo_ex_rt_wr_en;
+    logic [$bits(res_st_cell_t)+32-1:0] fifo_ex_rt_data_in;
+    logic fifo_ex_rt_empty_out;
+    logic fifo_ex_rt_full_out;
+
+    logic [31:0] retire_value_in;
+    res_st_cell_t retire_op_in;
+    logic phy_rf_rf_wr_en_out;
+    phy_rf_addr_t retire_phy_rf_wr_addr_out;
+    phy_rf_data_t retire_phy_rf_wr_data_out;
+    logic retire_busy_table_wr_en_out;
+    phy_rf_addr_t retire_busy_table_wr_addr_out;
+    logic retire_busy_table_wr_data_out;
+    rob_addr_t retire_rob_tail_ptr_out;
+    logic retire_rob_incr_tail_ptr_in;
+    logic retire_rob_full_out;
+    logic retire_res_st_retire_en_out;
+    rob_addr_t retire_res_st_retire_rob_addr_out;
+    phy_rf_data_t retire_res_st_retire_value_out;
+
     assign schedule_res_st_rd1_in = res_st_rd1_in;
     assign schedule_res_st_rd2_in = res_st_rd2_in;
     assign schedule_res_st_rd3_in = res_st_rd3_in;
@@ -67,12 +107,29 @@ module back_end
 
     assign execute_op_in = fifo_sh_ex_data_out;
 
+    assign fifo_ex_rt_rd_en = !fifo_sh_ex_empty_out;
+    assign fifo_ex_rt_wr_en = execute_op_out[0];
+    assign fifo_ex_rt_data_in = {execute_value_out, execute_op_out};
+
+    assign retire_value_in = fifo_ex_rt_data_out[($bits(fifo_ex_rt_data_out)-1) -: 32];
+    assign retire_op_in = fifo_ex_rt_data_out[$bits(res_st_cell_t)-1:0];
+    assign retire_rob_incr_tail_ptr_in = rob_incr_tail_ptr;
+
     assign res_st_rd1_addr = schedule_res_st_rd1_addr_out;
     assign res_st_rd2_addr = schedule_res_st_rd2_addr_out;
     assign res_st_rd3_addr = schedule_res_st_rd3_addr_out;
     assign res_st_rd4_addr = schedule_res_st_rd4_addr_out;
-    assign value_out = execute_value_out;
-    assign op_out = execute_op_out;
+    assign res_st_retire_en = retire_res_st_retire_en_out;
+    assign res_st_retire_rob_addr = retire_res_st_retire_rob_addr_out;
+    assign res_st_retire_value = retire_res_st_retire_value_out;
+    assign phy_rf_wr_en = retire_phy_rf_wr_en_out;
+    assign phy_rf_wr_addr = retire_phy_rf_wr_addr_out;
+    assign phy_rf_wr_data = retire_phy_rf_wr_data_out;
+    assign busy_table_wr_en = retire_busy_table_wr_en_out;
+    assign busy_table_wr_addr = retire_busy_table_wr_addr_out;
+    assign busy_table_wr_data = retire_busy_table_wr_data_out;
+    assign rob_tail_ptr = retire_rob_tail_ptr_out;
+    assign rob_full = retire_rob_full_out;
 
     schedule qu_schedule (
         .clk(clk),
@@ -110,6 +167,41 @@ module back_end
         .op_in(execute_op_in),
         .value_out(execute_value_out),
         .op_out(execute_op_out)
+    );
+
+    fifo #(
+        .FIFO_WIDTH($bits(res_st_cell_t)+32),
+        .FIFO_DEPTH(4)
+    ) qu_fifo_ex_rt (
+        .clk(clk),
+        .rst(rst),
+        .rd_en(fifo_ex_rt_rd_en),
+        .data_out(fifo_ex_rt_data_out),
+        .wr_en(fifo_ex_rt_wr_en),
+        .data_in(fifo_ex_rt_data_in),
+        .empty(fifo_ex_rt_empty_out),
+        .almost_empty(),
+        .full(fifo_ex_rt_full_out),
+        .almost_full()
+    );
+
+    retire qu_retire (
+        .clk(clk),
+        .rst(rst),
+        .value_in(retire_value_in),
+        .op_in(retire_op_in),
+        .phy_rf_wr_en(retire_phy_rf_wr_en_out),
+        .phy_rf_wr_addr(retire_phy_rf_wr_addr_out),
+        .phy_rf_wr_data(retire_phy_rf_wr_data_out),
+        .busy_table_wr_en(retire_busy_table_wr_en_out),
+        .busy_table_wr_addr(retire_busy_table_wr_addr_out),
+        .busy_table_wr_data(retire_busy_table_wr_data_out),
+        .rob_tail_ptr(retire_rob_tail_ptr_out),
+        .rob_incr_tail_ptr(retire_rob_incr_tail_ptr_in),
+        .rob_full(retire_rob_full_out),
+        .res_st_retire_en(retire_res_st_retire_en_out),
+        .res_st_retire_rob_addr(retire_res_st_retire_rob_addr_out),
+        .res_st_retire_value(retire_res_st_retire_value_out)
     );
 
 endmodule
--- hdl/sim/tb_back_end.sv
+++ hdl/sim/tb_back_end.sv
@@ -1,6 +1,6 @@
 // Testbench for the top back-end module of The Qu Processor
 // Created:     2025-07-03
-// Modified:    2025-07-04
+// Modified:    2025-07-06
 
 // Copyright (c) 2025 Kagan Dikmen
 // SPDX-License-Identifier: MIT
@@ -24,8 +24,21 @@ module tb_back_end
     res_st_addr_t res_st_rd4_addr;
     res_st_cell_t res_st_rd4_in;
 
-    logic [31:0] value_out;
-    res_st_cell_t op_out;
+    logic res_st_retire_en;
+    rob_addr_t res_st_retire_rob_addr;
+    phy_rf_data_t res_st_retire_value;
+
+    logic phy_rf_wr_en;
+    phy_rf_addr_t phy_rf_wr_addr;
+    phy_rf_data_t phy_rf_wr_data;
+
+    logic busy_table_wr_en;
+    phy_rf_addr_t busy_table_wr_addr;
+    logic busy_table_wr_data;
+
+    rob_addr_t rob_tail_ptr;
+    logic rob_incr_tail_ptr;
+    logic rob_full;
 
     back_end dut (
         .clk(clk),
@@ -39,8 +52,18 @@ module tb_back_end
         .res_st_rd3_in(res_st_rd3_in),
         .res_st_rd4_addr(res_st_rd4_addr),
         .res_st_rd4_in(res_st_rd4_in),
-        .value_out(value_out),
-        .op_out(op_out)
+        .res_st_retire_en(res_st_retire_en),
+        .res_st_retire_rob_addr(res_st_retire_rob_addr),
+        .res_st_retire_value(res_st_retire_value),
+        .phy_rf_wr_en(phy_rf_wr_en),
+        .phy_rf_wr_addr(phy_rf_wr_addr),
+        .phy_rf_wr_data(phy_rf_wr_data),
+        .busy_table_wr_en(busy_table_wr_en),
+        .busy_table_wr_addr(busy_table_wr_addr),
+        .busy_table_wr_data(busy_table_wr_data),
+        .rob_tail_ptr(rob_tail_ptr),
+        .rob_incr_tail_ptr(rob_incr_tail_ptr),
+        .rob_full(rob_full)
     );
 
     always #5   clk = ~clk;
@@ -50,7 +73,10 @@ module tb_back_end
         clk <= 1'b0;
         rst <= 1'b0;
         schedule_en <= 1'b1;
+        rob_incr_tail_ptr <= 1'b1;
 
+        res_st_rd1_in.rob_addr <= 2'd0;
+        res_st_rd1_in.dest <= 'd4;
         res_st_rd1_in.busy <= 1'b1;
         res_st_rd1_in.op <= 14'd1;
         res_st_rd1_in.qj <= 'd0;
@@ -59,24 +85,30 @@ module tb_back_end
         res_st_rd1_in.vk <= 'd12;
         res_st_rd1_in.a <= 'd0;
 
+        res_st_rd2_in.rob_addr <= 2'd1;
+        res_st_rd2_in.dest <= 'd5;
         res_st_rd2_in.busy <= 1'b1;
-        res_st_rd2_in.op <= 14'd2;
+        res_st_rd2_in.op <= 14'd3;
         res_st_rd2_in.qj <= 'd0;
         res_st_rd2_in.qk <= 'd0;
         res_st_rd2_in.vj <= 'd21;
         res_st_rd2_in.vk <= 'd22;
         res_st_rd2_in.a <= 'd0;
 
+        res_st_rd3_in.rob_addr <= 2'd2;
+        res_st_rd3_in.dest <= 'd6;
         res_st_rd3_in.busy <= 1'b1;
-        res_st_rd3_in.op <= 14'd3;
+        res_st_rd3_in.op <= 14'd5;
         res_st_rd3_in.qj <= 'd0;
         res_st_rd3_in.qk <= 'd0;
         res_st_rd3_in.vj <= 'd31;
         res_st_rd3_in.vk <= 'd32;
         res_st_rd3_in.a <= 'd0;
 
+        res_st_rd4_in.rob_addr <= 2'd3;
+        res_st_rd4_in.dest <= 'd7;
         res_st_rd4_in.busy <= 1'b1;
-        res_st_rd4_in.op <= 14'd4;
+        res_st_rd4_in.op <= 14'd7;
         res_st_rd4_in.qj <= 'd0;
         res_st_rd4_in.qk <= 'd0;
         res_st_rd4_in.vj <= 'd41;
@@ -90,6 +122,8 @@ module tb_back_end
         rst <= 1'b0;
 
         repeat(4) @(posedge clk);
+        res_st_rd1_in.rob_addr <= 2'd0;
+        res_st_rd1_in.dest <= 'd4;
         res_st_rd1_in.busy <= 1'b1;
         res_st_rd1_in.op <= 14'd1;
         res_st_rd1_in.qj <= 'd0;
@@ -98,24 +132,30 @@ module tb_back_end
         res_st_rd1_in.vk <= 'd12;
         res_st_rd1_in.a <= 'd0;
 
+        res_st_rd2_in.rob_addr <= 2'd1;
+        res_st_rd2_in.dest <= 'd5;
         res_st_rd2_in.busy <= 1'b1;
-        res_st_rd2_in.op <= 14'd2;
+        res_st_rd2_in.op <= 14'd3;
         res_st_rd2_in.qj <= 'd1;
         res_st_rd2_in.qk <= 'd0;
         res_st_rd2_in.vj <= 'd21;
         res_st_rd2_in.vk <= 'd22;
         res_st_rd2_in.a <= 'd0;
 
+        res_st_rd3_in.rob_addr <= 2'd2;
+        res_st_rd3_in.dest <= 'd6;
         res_st_rd3_in.busy <= 1'b1;
-        res_st_rd3_in.op <= 14'd3;
+        res_st_rd3_in.op <= 14'd5;
         res_st_rd3_in.qj <= 'd0;
         res_st_rd3_in.qk <= 'd0;
         res_st_rd3_in.vj <= 'd31;
         res_st_rd3_in.vk <= 'd32;
         res_st_rd3_in.a <= 'd0;
 
+        res_st_rd4_in.rob_addr <= 2'd3;
+        res_st_rd4_in.dest <= 'd7;
         res_st_rd4_in.busy <= 1'b1;
-        res_st_rd4_in.op <= 14'd4;
+        res_st_rd4_in.op <= 14'd7;
         res_st_rd4_in.qj <= 'd0;
         res_st_rd4_in.qk <= 'd0;
         res_st_rd4_in.vj <= 'd41;
@@ -127,12 +167,15 @@ module tb_back_end
 
         repeat(10) @(posedge clk);
         schedule_en <= 1'b0;
+        rob_incr_tail_ptr <= 1'b0;
 
         repeat(3) @(posedge clk);
         schedule_en <= 1'b1;
+        rob_incr_tail_ptr <= 1'b1;
 
         repeat(3) @(posedge clk);
         schedule_en <= 1'b0;
+        rob_incr_tail_ptr <= 1'b0;
 
         #100;
         $finish;
